5484c3ecfe3d617285984fdf62dda5fab8c77f8d
Saif-ur-Rehman <saif.ur.rehman@devtrongenesis.com>
2016-10-22 17:58:22 +0500

    views and menu modules

--- sites/all/themes/business/templates/page.tpl.php
+++ sites/all/themes/business/templates/page.tpl.php
@@ -100,7 +100,7 @@
       <div class="navbar-collapse collapse">
         <nav role="navigation">
           <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
+            <?php // print render($primary_nav); ?>
           <?php endif; ?>
           <?php if (!empty($secondary_nav)): ?>
             <?php print render($secondary_nav); ?>
